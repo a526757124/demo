e262300bf345e187f84739d149a15bce0db27d37
a526757124 <lijun526757124@163.com>
2015-01-20 18:03:20 +0800

    12

---
+++ CTS/Areas/SystemSettings/SystemSettingsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CTS.Areas.SystemSettings
+{
+    public class SystemSettingsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "SystemSettings";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "SystemSettings_default",
+                "SystemSettings/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
