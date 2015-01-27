79b25db359e1752b56955c3f291b5c7dc8079d4c
a526757124 <lijun526757124@163.com>
2015-01-27 09:04:53 +0800

    13

---
+++ ETVS.Framework.Service.Core/IUnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ETVS.Framework.Service.Core
+{
+    /// <summary>
+    /// 业务单元操作接口
+    /// </summary>
+    public interface IUnitOfWork : IDependency
+    {
+        #region 属性
+
+        /// <summary>
+        /// 获取或设置 是否开启事务提交
+        /// </summary>
+        bool TransactionEnabled { get; set; }
+
+        #endregion
+
+        #region 方法
+
+        /// <summary>
+        /// 提交当前单元操作的更改。
+        /// </summary>
+        /// <returns>操作影响的行数</returns>
+        int SaveChanges();
+
+#if NET45
+
+        /// <summary>
+        /// 异步提交当前单元操作的更改。
+        /// </summary>
+        /// <returns>操作影响的行数</returns>
+        Task<int> SaveChangesAsync();
+
+#endif
+
+        #endregion
+    }
+}
