e964308902f386d0da36585627abb3df2db3cd2c
a526757124 <lijun526757124@163.com>
2015-03-17 17:58:16 +0800

    12

---
+++ DomainModels/Company.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DomainModels
+{
+
+    /// <summary>
+    /// 公司
+    /// </summary>
+    public class Company
+    {
+        /// <summary>
+        /// 公司名称
+        /// </summary>
+        public string CompanyName { get; set; }
+        /// <summary>
+        /// 公司联系人
+        /// </summary>
+        public string CompanyContact { get; set; }
+        /// <summary>
+        /// 公司联系电话
+        /// </summary>
+        public string CompanyPhone { get;set;}
+        /// <summary>
+        /// 公司地址
+        /// </summary>
+        public string CompanyAddress { get; set; }
+
+
+    }
+}
---
+++ DomainModels/User.cs
@@ -0,0 +1,27 @@
+﻿using ETVS.Framework.Entity.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DomainModels
+{
+    /// <summary>
+    /// 用户
+    /// </summary>
+    public class User:Operator
+    {
+        /// <summary>
+        /// 用户姓名
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 用户登录名
+        /// </summary>
+        public string LoginName { get; set; }
+        /// <summary>
+        /// 登录密码
+        /// </summary>
+        public string Pwd { get; set; }
+    }
+}
